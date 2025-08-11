cc3d0ff9f86ff8da6a2cf4ddd25a758b2e9f335d
Lucas Georges Gabriel Charpentier Pacheco <lgcharpe@gmail.com>
2025-08-11 15:21:21 +0200

    [README] Completing the README for submission

--- eval_zero_shot_fast_all_revisions.sh
+++ eval_zero_shot_fast_all_revisions.sh
@@ -8,13 +8,13 @@ EVAL_DIR=${4:-"evaluation_data/fast_eval"}
 for i in {1..9}; do
     checkpoint="chck_${i}M"
     echo "Evaluating checkpoint ${checkpoint}"
-    #bash eval_zero_shot_fast.sh $MODEL_PATH $checkpoint $BACKEND $EVAL_DIR
+    bash eval_zero_shot_fast.sh $MODEL_PATH $checkpoint $BACKEND $EVAL_DIR
 done
 
 for i in {10..100..10}; do
     checkpoint="chck_${i}M"
     echo "Evaluating checkpoint ${checkpoint}"
-    #bash eval_zero_shot_fast.sh $MODEL_PATH $checkpoint $BACKEND $EVAL_DIR
+    bash eval_zero_shot_fast.sh $MODEL_PATH $checkpoint $BACKEND $EVAL_DIR
 done
 
 # Conditional on whether the track is strict-small
@@ -22,6 +22,6 @@ if [[ "$3" != "strict-small" ]]; then
     for i in {200..1000..100}; do
 	checkpoint="chck_${i}M"
 	echo "Evaluating checkpoint ${checkpoint}"
-	#bash eval_zero_shot_fast.sh $MODEL_PATH $checkpoint $BACKEND $EVAL_DIR
+	bash eval_zero_shot_fast.sh $MODEL_PATH $checkpoint $BACKEND $EVAL_DIR
     done
 fi
